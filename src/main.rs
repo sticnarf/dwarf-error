92d195e8e05118e76c68f5b31ca89e6cd4bd45f6
Yilin Chen <sticnarf@gmail.com>
2021-09-12 00:18:07 +0800

    try to minimize

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
 use tokio::runtime::Builder;
-use tokio::runtime::Runtime;
-use tonic::codegen::*;
+use tonic::client::Grpc;
+use tonic::codec::ProstCodec;
+use tonic::codegen::http::uri::PathAndQuery;
+use tonic::transport::Channel;
 use tonic::IntoRequest;
 
 #[derive(::prost::Message)]
-pub struct GetRequest {}
-
-#[derive(::prost::Message)]
-pub struct GetResponse {}
+pub struct Proto;
 
 fn main() {
-    let future = async {
-        let conn = tonic::transport::Endpoint::new("http://[::1]:50051")
-            .unwrap()
-            .connect()
-            .await
-            .unwrap();
-        let mut client = tonic::client::Grpc::new(conn);
-        let req = GetRequest::default();
-        client.ready().await.unwrap();
-        let codec = tonic::codec::ProstCodec::default();
-        let path = http::uri::PathAndQuery::from_static("/tikvpb.Tikv/KvGet");
-        let res: Result<tonic::Response<GetResponse>, tonic::Status> =
-            client.unary(req.into_request(), path, codec).await;
+    let f = async {
+        let client = unsafe { &mut *(0x1usize as *mut Grpc<Channel>) };
+        let _: Result<tonic::Response<Proto>, tonic::Status> = client
+            .unary(
+                Proto.into_request(),
+                PathAndQuery::from_static(""),
+                ProstCodec::<Proto, Proto>::default(),
+            )
+            .await;
     };
     let rt = Builder::new_current_thread().build().unwrap();
-    rt.block_on(future);
+    rt.block_on(f);
 }
